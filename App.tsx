904775871e5449f0817bece0cea4526647dfe7c5
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2023-04-29 14:00:31 -0300

    chore: exporting items to a separated file

--- App.tsx
+++ App.tsx
@@ -2,66 +2,9 @@ import { SortableGallery } from './src';
 import { StyleSheet } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { Image } from 'types';
+import { items } from './items';
 
 export default function App() {
-  const items = [
-    {
-      id: '1',
-      file_url: 'https://picsum.photos/200',
-      order: 1,
-    },
-    {
-      id: '2',
-      file_url: 'https://picsum.photos/200',
-      order: 2,
-    },
-    {
-      id: '3',
-      file_url: 'https://picsum.photos/200',
-      order: 3,
-    },
-    {
-      id: '4',
-      file_url: 'https://picsum.photos/200',
-      order: 4,
-    },
-    {
-      id: '5',
-      file_url: 'https://picsum.photos/200',
-      order: 5,
-    },
-    {
-      id: '6',
-      file_url: 'https://picsum.photos/200',
-      order: 6,
-    },
-    {
-      id: '7',
-      file_url: 'https://picsum.photos/200',
-      order: 7,
-    },
-    {
-      id: '8',
-      file_url: 'https://picsum.photos/200',
-      order: 8,
-    },
-    {
-      id: '9',
-      file_url: 'https://picsum.photos/200',
-      order: 9,
-    },
-    {
-      id: '10',
-      file_url: 'https://picsum.photos/200',
-      order: 10,
-    },
-    {
-      id: '11',
-      file_url: 'https://picsum.photos/200',
-      order: 11,
-    },
-  ];
-
   function handleDragEnd(item: Image) {
     console.log('handleDragEnd', item);
   }
---
+++ items.ts
@@ -0,0 +1,57 @@
+export const items = [
+  {
+    id: '1',
+    file_url: 'https://picsum.photos/200',
+    order: 1,
+  },
+  {
+    id: '2',
+    file_url: 'https://picsum.photos/200',
+    order: 2,
+  },
+  {
+    id: '3',
+    file_url: 'https://picsum.photos/200',
+    order: 3,
+  },
+  {
+    id: '4',
+    file_url: 'https://picsum.photos/200',
+    order: 4,
+  },
+  {
+    id: '5',
+    file_url: 'https://picsum.photos/200',
+    order: 5,
+  },
+  {
+    id: '6',
+    file_url: 'https://picsum.photos/200',
+    order: 6,
+  },
+  {
+    id: '7',
+    file_url: 'https://picsum.photos/200',
+    order: 7,
+  },
+  {
+    id: '8',
+    file_url: 'https://picsum.photos/200',
+    order: 8,
+  },
+  {
+    id: '9',
+    file_url: 'https://picsum.photos/200',
+    order: 9,
+  },
+  {
+    id: '10',
+    file_url: 'https://picsum.photos/200',
+    order: 10,
+  },
+  {
+    id: '11',
+    file_url: 'https://picsum.photos/200',
+    order: 11,
+  },
+];
